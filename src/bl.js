f3e0fd9821a07ec6df9a1c83fc5eab8a9de9fb9d
Timo Tiuraniemi <timo.tiuraniemi@iki.fi>
2016-02-11 13:17:52 +0200

    Added separate file for business logic

---
+++ src/bl.js
@@ -0,0 +1,20 @@
+'use strict'
+
+const MongoClient = require('mongodb').MongoClient;
+const ObjectId = require('mongodb').ObjectID;
+
+module.exports = (mongodbUrl) => {
+
+  async function getQuestionnaires() {
+    // TODO: Mongodb-kutsu
+    return new Promise(function(resolve, reject){
+      setTimeout(function(){
+        resolve("Hello World");
+      }, 1000)
+    });
+  }
+
+  return {
+    getQuestionnaires: getQuestionnaires
+  };
+}
--- src/interface.js
+++ src/interface.js
@@ -1,16 +1,16 @@
 'use strict'
 
-const MongoClient = require('mongodb').MongoClient;
-const ObjectId = require('mongodb').ObjectID;
+// TODO: Require JSON Schema here
 
 module.exports = (mongodbUrl) => {
 
+  const bl = require('./bl.js')(mongodbUrl);
+
   async function getQuestionnaires() {
-    return new Promise(function(resolve, reject){
-      setTimeout(function(){
-        resolve("Hello World");
-      }, 100)
-    });
+    let questionnaires = await bl.getQuestionnaires();
+    // TODO: Perhaps validate also outgoing response?
+    // validateSchmema(quesitonnaires, quesionnairesSchema);
+    return questionnaires;
   }
 
   return {
