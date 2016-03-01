e557aa4a385edd470bd5e32a02003e3e5c2a3ca8
Tomi Piri <tomi.piri@tomipiri.me>
2016-03-01 14:01:56 +0200

    Add generate UUID and timestamp to putQuestionnaire

--- src/interface.js
+++ src/interface.js
@@ -2,7 +2,7 @@
 
 // TODO: Require JSON Schema here
 const Ajv = require('ajv');
-const schema = require('./questionnaireschema.json');
+const schema = require('./questionnaire.schema.json');
 const ajv = Ajv(); // options can be passed, e.g. {allErrors: true}
 const validate = ajv.compile(schema);
 //var valid = validate(data);
@@ -11,15 +11,63 @@ const validate = ajv.compile(schema);
 module.exports = (mongodbUrl) => {
 
   const bl = require('./bl.js')(mongodbUrl);
-
+  
+  function createUuid(){
+    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+      var r = Math.random()*16|0, v = c === 'x' ? r : (r&0x3|0x8);
+      return v.toString(16);
+    });
+  }
+  
+  /*
+  Generates UUID and Timestamps for a new questionnaire
+  */
+  function generateUuidsAndTimestamps(payload){
+  	//let genSchema = require('./generator.schema.json');
+  	//let genValidate = ajv.compile(genSchema);
+    //if(genValidate(payload)){  //Option 1 for validating payload before generating, Using schema
+  	
+    //if('uuid' in payload &&    //Option 2 for validating payload before generating, Using if 
+  	//   'created' in payload && 
+  	//   'modified' in payload && 
+  	//   'pages' in payload){ // Check if payload has required properties
+  	  let currentTime = Date.now();
+  	  payload.created = currentTime; 
+  	  payload.modified = currentTime;
+  	
+  	  payload.uuid = createUuid(); // Generate UUID for Questionnaire
+  	  
+  	  for(let i = 0;i < payload.pages.length; i++){ // Go through all pages
+  	  	//if('elements' in payload.pages[i]){ // Check if a page has 'elements'
+  		  let pageElements = payload.pages[i].elements; // Get elements of a page
+  		  for(let a = 0; a < pageElements.length; a++){ // Go through all elements
+  		  	//if('uuid' in pageElements[a]){ // Check if an element has 'uuid'
+  		      pageElements[a].uuid = createUuid(); // Generate UUID for an element
+  		      if ('options' in pageElements[a]){ // Check if an element has options
+  		        let elementOptions = pageElements[a].options; // Get options of an element
+  			    for(let e = 0; e < elementOptions.length;e++){ // Go through all options
+  			      //if('uuid' in elementOptions[e]){ // Check if an option has 'uuid'
+  			        elementOptions[e].uuid = createUuid(); // Generate UUID for an option
+  			      //}
+  			    }
+  		      }
+  		    //}
+  		  }
+  	    //}
+  	  }
+  	}
+  	return payload;
+  }
+  
   async function getQuestionnaires() {
     let questionnaires = await bl.getQuestionnaires();
     // TODO: Perhaps validate also outgoing response?
     // validateSchmema(quesitonnaires, quesionnairesSchema);
     return questionnaires;
   }
+  
   async function putQuestionnaire(payload) {
-  	
+  	payload = generateUuidsAndTimestamps(payload)
   	
   	let valid = validate(payload);
   	let responseFromBl = false;
