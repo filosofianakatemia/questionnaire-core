b60bd6709441fc75c14aa90669aefe4dc443d15a
Tomi Piri <tomi.piri@tomipiri.me>
2016-03-08 12:05:09 +0200

    Add getQuestionnaires functionality, Fix putQuestionnaire

--- src/bl.js
+++ src/bl.js
@@ -8,11 +8,26 @@ module.exports = (mongodbUrl) => {
   async function getQuestionnaires() {
     // TODO: Mongodb-kutsu
     return new Promise(function(resolve, reject){
-      setTimeout(function(){
-        resolve("Hello World");
-      }, 1000)
+      let findQuestionnaires = function(db, callback) {
+      	let resultsArray = new Array();
+        let cursor = db.collection('questionnaires').find( );
+        cursor.each(function(err, doc) {
+          if (doc !== null) {
+          	 resultsArray.push(doc);
+          } else {
+            callback(resultsArray);
+          }
+        });
+      };
+      MongoClient.connect(mongodbUrl, function(err, db) {
+        findQuestionnaires(db, function(resultsArray) {
+          db.close();
+          resolve(JSON.stringify(resultsArray));
+        });
+      });
     });
   }
+  
   async function putQuestionnaire(payload) {
     return new Promise(function(resolve, reject){
       let insertQuestionnaire = function(db, callback) {
@@ -24,14 +39,14 @@ module.exports = (mongodbUrl) => {
       MongoClient.connect(mongodbUrl, function(err, db) {
         insertQuestionnaire(db, function() {
           db.close();
+          
+          let returnJson = new Object();
+          returnJson.uuid = payload.uuid;
+          returnJson.created = payload.created;
+          returnJson.modified = payload.modified;
+          resolve(JSON.stringify(returnJson));
         });
       });
-      
-      let returnJson = new Object();
-      returnJson.uuid = payload.uuid;
-      returnJson.created = payload.created;
-      returnJson.modified = payload.modified;
-      resolve(JSON.stringify(returnJson));
     });
   }
 
